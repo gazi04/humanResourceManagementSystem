b3b1409c8688e805adf86fd412982a21e58feecb
gazi04 <gazmendhalili2016@gmail.com>
2025-03-13 12:42:34 +0100

    :seedling: added a route to populate database with dummy data

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,26 @@ use App\Models\Role;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
-Route::get('/', [EmployeeController::class, 'index'])->name('dashboard')->middleware([EnsureUserIsLoggedIn::class, IsUserHR::class]);
+Route::get('/', [EmployeeController::class, 'index'])->name('dashboard')->middleware([EnsureUserIsLoggedIn::class]);
 Route::get('/login', [LoginController::class, 'showLoginPage'])->name('loginPage');
 Route::post('/authenticate', [LoginController::class, 'login'])->name('login');
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+
+
+Route::get('/dummy-data', function() {
+    $admin = Employee::create([
+        'firstName' => 'gazi',
+        'lastName' => 'gazi',
+        'email' => 'gazi@gmail.com',
+        'password' => Hash::make('gazigazi'),
+        'phone' => '045681376'
+    ]);
+
+    $role = Role::create(['roleName' => 'admin']);
+    $employeeRole = EmployeeRole::create([
+        'employeeID' => $admin['employeeID'],
+        'roleID' => $role['roleID']
+    ]);
+
+    return redirect()->route('loginPage');
+});
