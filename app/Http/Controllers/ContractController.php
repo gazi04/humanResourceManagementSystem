7ea4f820c7796f88f524083a9ad506db8a33d007
gazi04 <gazmendhalili2016@gmail.com>
2025-03-31 19:04:46 +0200

    :sparkles: integrate contract service update and delete methods into contract controller
    This commit implements the `update` and `delete` methods from the
    contract service within the contract controller. This allows the
    controller to utilize the service's logic for updating and deleting
    contracts, including database and local storage management.
    Needs to be tested.

--- app/Http/Controllers/ContractController.php
+++ app/Http/Controllers/ContractController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Contract\DeleteContractRequest;
 use App\Http\Requests\Contract\DownloadContractRequest;
 use App\Http\Requests\Contract\GetEmployeeContractsRequest;
+use App\Http\Requests\Contract\UpdateContractRequest;
 use App\Http\Requests\Contract\UploadContractRequest;
 use App\Models\Contract;
 use App\Models\Employee;
@@ -22,7 +24,7 @@ class ContractController extends Controller
         try {
             return $this->contractService->getEmployeeContracts($employee);
         } catch (\Exception $e) {
-            Log::error('Gjatë ngarkimit të kontratës ndodhi ky gabim: ', [$e->getMessage()]);
+            Log::error('Gjatë ngarkimit të kontratave ndodhi ky gabim: ', [$e->getMessage()]);
 
             return back()->with('error', 'Ndodhi një gabim në sistem me marrjen e kontratave, provoni përsëri më vonë.');
         }
@@ -54,9 +56,40 @@ class ContractController extends Controller
         try {
             return $this->contractService->downloadContract($contract);
         } catch (\Exception $e) {
-            Log::error('Gjatë ngarkimit të kontratës ndodhi ky gabim: ', [$e->getMessage()]);
+            Log::error('Gjatë shkarkimit të kontratës ndodhi ky gabim: ', [$e->getMessage()]);
 
             return back()->with('error', 'Ndodhi një gabim në sistem me ngarkimin e kontratës, provoni përsëri më vonë.');
+        }
+    }
+
+    public function update(UpdateContractRequest $request)
+    {
+        $validated = $request->only('employeeID', 'contractID', 'contract_file');
+
+        $employee = Employee::where('employeeID', $validated['employeeID'])->first();
+        $contract = Contract::where('contractID', $validated['contractID'])->first();
+
+        try {
+            $this->contractService->updateContract($contract, $employee, $validated);
+
+        } catch (\Exception $e) {
+            Log::error('Gjatë përditësimit të kontratës ndodhi ky gabim: ', [$e->getMessage()]);
+
+            return back()->with('error', 'Ndodhi një gabim në sistem me përditësimit të kontratës, provoni përsëri më vonë.');
+        }
+    }
+
+    public function delete(DeleteContractRequest $request)
+    {
+        $validated = $request->only('contractID');
+        $contract = Contract::where('contractID', $validated['contractID'])->first();
+
+        try {
+            return $this->contractService->deleteContract($contract);
+        } catch (\Exception $e) {
+            Log::error('Gjatë fshirjes së kontratës ndodhi ky gabim: ', [$e->getMessage()]);
+
+            return back()->with('error', 'Ndodhi një gabim në sistem me fshirjen e kontratës, provoni përsëri më vonë.');
         }
     }
 }
