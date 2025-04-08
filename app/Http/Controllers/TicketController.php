0dc03087868f7219d0588c0e9c064b8225256a7d
gazi04 <gazmendhalili2016@gmail.com>
2025-04-08 11:16:27 +0200

    :zap: improved create method by adding redirect if exception is catched

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -30,6 +30,16 @@ class TicketController extends Controller
 
     public function create(CreateTicketRequest $request)
     {
+        if ($request->is('hr/*')) {
+            $route = 'hr.dashboard';
+        }
+        else if ($request->is('employee/*')) {
+            $route = 'employee.dashboard';
+        }
+        else if ($request->is('manager/*')) {
+            $route = 'manager.dashboard';
+        }
+
         try {
             $validated = $request->only('subject', 'description');
             /** @var Employee $user */
@@ -42,28 +52,18 @@ class TicketController extends Controller
                 return redirect()->back()->with('error', 'Procesi i krijimit të biletës ka dështuar provo më vonë përsëri.');
             }
 
-            if ($request->is('hr/*')) {
-                $route = 'hr.dashboard';
-            }
-            else if ($request->is('employee/*')) {
-                $route = 'employee.dashboard';
-            }
-            else if ($request->is('manager/*')) {
-                $route = 'employee.dashboard';
-            }
-
             return redirect()->route($route)
                 ->with('success', 'Bileta u krijua me sukses.');
-
-        } catch (\Illuminate\Database\QueryException $e) {
-            Log::error('Krijimi i biletës dështoi për shkak të gabimit të bazës së të dhënave.',
-                ['error' => config('app.debug') ? $e->getMessage() : 'Database error occurred']);
         } catch (\PDOException $e) {
             Log::error('Problemi i lidhjes me bazën e të dhënave.',
                 ['error' => config('app.debug') ? $e->getMessage() : 'Service unavailable']);
+            return redirect()->route($route)
+                ->with('error', 'Procesi i krijimit të biletës ka dështuar provo më vonë përsëri.');
         } catch (\Exception $e) {
             Log::error('Krijimi i biletës dështoi.',
                 ['error' => config('app.debug') ? $e->getMessage() : 'An error occurred']);
+            return redirect()->route($route)
+                ->with('error', 'Procesi i krijimit të biletës ka dështuar provo më vonë përsëri.');
         }
     }
 
